ce44f63251cfa36171dbde0dde3d73d961abab57
Tiago Deliberali <tiago.deliberali@gmail.com>
2020-04-26 17:55:36 -0300

    refactor content format function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::char;
 use std::io;
 use std::io::prelude::*;
-use std::char;
 
 const LINE_SIZE: usize = 16;
 
@@ -11,33 +11,37 @@ fn main() {
 }
 
 pub fn parse<T: BufRead + Sized>(mut data: T) {
-    let mut buffer= [0; LINE_SIZE];
+    let mut buffer = [0; LINE_SIZE];
     let mut pos: usize = 0;
 
     while let Ok(_) = data.read_exact(&mut buffer) {
         print!("[{:016x}] ", pos);
-        let mut content = vec!();
-
         for i in 0..LINE_SIZE {
             print!("{:02x} ", buffer[i]);
             if (i + 1) % 8 == 0 && i != 0 {
                 print!(" ");
             }
         }
-
-        for _ in 0..LINE_SIZE {
-            content.push(String::from("."));
-        }
-
-        for x in String::from_utf8_lossy(&buffer).match_indices(char::is_alphanumeric) {
-            if x.0 >= LINE_SIZE {
-                for _ in content.len()..=x.0 {
-                    content.push(String::from("."));
-                }
-            }
-            content[x.0] = String::from(x.1);
-        }
-        println!(" |{}|", content.join(""));
+        println!(" |{}|", build_content(&buffer));
         pos += LINE_SIZE;
     }
+}
+
+fn build_content(buffer: &[u8]) -> String {
+    let mut content = vec![];
+
+    for _ in 0..LINE_SIZE {
+        content.push(String::from("."));
+    }
+
+    for x in String::from_utf8_lossy(&buffer).match_indices(char::is_alphanumeric) {
+        if x.0 >= LINE_SIZE {
+            for _ in content.len()..=x.0 {
+                content.push(String::from("."));
+            }
+        }
+        content[x.0] = String::from(x.1);
+    }
+
+    content.join("")
 }
